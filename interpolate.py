8d61ea8650c799ec65a38882721c97bc8d386fce
Junhee Kim <102019063+joon0390@users.noreply.github.com>
2024-01-02 16:45:32 +0900

    Add files via upload
    데이터셋 코드와 데이터 보간 코드입니다

---
+++ interpolate.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy.interpolate import UnivariateSpline
+
+def interpolate_data(uptime, data, new_interval=1e-5):
+    try:
+        f = UnivariateSpline(uptime, data, k=1)
+        interpolated_data = f(uptime)  # 보간된 값을 계산
+        return interpolated_data
+    except Exception as e:
+        print("Error occurred during interpolation:", e)
+        return None
