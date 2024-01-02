8c3741ff18b190c43fdfdcbf29c6860f22f50f2a
shk118 <127921840+Resias@users.noreply.github.com>
2024-01-02 19:37:37 +0900

    Upload validate part a little
    미완성입니다
    남은 작업 목록
    data_root
    dataset, dataloader
    n_fft
    hop_length
    batchsize
    threshold

--- model.py
+++ model.py
@@ -53,10 +53,67 @@ class CPD_SSL():
             print(f'Error : Unspportable Backbone - {backbone}')
 
     def train_one_epoch(self):
-        
+
+        # 미완성
     def valid_one_epoch(self):
+        data_root = ""
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
         
+        global best_acc
+        self.backbone.eval() 
         
+        true_correct = 0
+        false_correct = 0
+        total = 0
+        
+        hop_length = 0
+        n_fft = 0
+        
+        batch_size = 0
+        setting_threshold = 0.0
+        
+        dataset = UntrimmedDataset(root_dir=data_root,
+                               kernel_size=512,
+                               stride=256,
+                               device=device,
+                               n_fft=n_fft,
+                               hop_length=hop_length)
+    
+        dataloader = DataLoader(dataset, batch_size=4)
+        with torch.no_grad():
+            for index, batches in enumerate(dataloader):
+                inputs, labels, is_new = batches
+                inputs = inputs.to(device)
+                predicted = self.backbone(inputs)
+                
+                for idx in range(batch_size):               # 각 배치별
+                    if predicted[idx] < setting_threshold:  # 해당 배치가 threshold 이하인지
+                        tf_flag = True
+                        
+                        l0 = labels[idx][0]
+                        # l0 = len(set(labels[idx].unique().numpy()))
+                        
+                        for l in labels[idx]:               # 실제 CP인지 확인
+                            if l0 != l: break
+                            else:
+                                tf_flag = False
+                                break
+                        """
+                        if l0 > 1:
+                            true_correct += 1
+                        else:
+                            false_correct += 1
+                        """
+                        
+                        if tf_flag:
+                            true_correct += 1
+                        else:
+                            false_correct += 1
+                
+                total += predicted.size(0)
+                # correct += (predicted == targets).sum().item()
+            
+            print(f'[Test] epoch: {1} | Acc: {true_correct / total * 100:.4f}')
 
 
 
