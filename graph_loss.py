9426a35c71b63d76b258acc323dff3384bf09476
Junhee Kim <kim8881472@gmail.com>
2024-01-05 15:58:48 +0900

    Save Loss Graph

--- graph_loss.py
+++ graph_loss.py
@@ -12,26 +12,24 @@ stride = '32'
 #------------------------
 
 file_path =  os.path.join(root,"outputs",model,"result.json")
-
-# JSON 파일 불러오기
+new_folder = os.path.join(root,"outputs",model,'loss_graph')
+os.makedirs(new_folder)
+            
 with open(file_path, 'r') as file:
     data = json.load(file)
 
-# 각각의 데이터에서 loss_epoch 값 추출
 epochs = [entry["Epoch"] for entry in data]
 loss_values = [entry["loss_epoch"] for entry in data]
 
-# 시각화
 plt.figure(figsize=(8, 6))
 plt.plot(epochs, loss_values, marker='o', linestyle='-',markersize=1)
 #plt.title('Model : {}'.format(model))
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
 plt.grid(True)
-# 범례에 모델 정보 추가
 model_info = "Model:" + model +"\nBatch Size : " + batch + "\nKernel Size : " + kernel_size + "\nStride : " + stride
 
-# 범례에 텍스트로 모델 정보 추가
 plt.legend([model_info], loc='upper right')
+os.chdir(new_folder)
 plt.savefig(model+'.png',bbox_inches='tight')
 plt.show()
